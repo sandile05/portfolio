cb987ea04087b15c801ab086714d0e81e7bbd8de
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-03-06 19:00:26 +0200

    updated menu toggle

--- js/script.js
+++ js/script.js
@@ -9,5 +9,6 @@ $(document).ready(function(){
     //toggle navbar
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     })
 })
